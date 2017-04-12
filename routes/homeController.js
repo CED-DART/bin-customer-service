d058292bc171f27c714e621d075b6f533d8a8c80
Botelho <reginaldo.botelho@dartdigital.com.br>
2017-04-12 19:53:05 -0300

    get edit

--- routes/homeController.js
+++ routes/homeController.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render('pages/index', {data: data});
+    res.render('pages/index', { data: data });
 });
 
 router.get("/create", (req, res) => {
@@ -15,11 +15,11 @@ router.get("/create", (req, res) => {
 
 router.post("/create", (req, res) => {
     // var jsonDatabase = fs.createReadStream('data.json');
-    
+
     let nextId = 1;
     data.map((row, index) => {
         let id = Number.parseInt(row.id);
-        if(id > nextId){
+        if (id > nextId) {
             nextId = id;
         }
     });
@@ -27,8 +27,9 @@ router.post("/create", (req, res) => {
     var model = new customerService();
     model.id = (nextId + 1).toString();
     model.title = req.body.title;
+    model.company = req.body.company;
     model.description = req.body.description;
-    model.resolved = req.body.resolved;
+    model.resolved = req.body.resolved ? true : false;
     model.promiseSolutionDate = req.body.promiseSolutionDate;
 
     data.push(model);
@@ -37,11 +38,23 @@ router.post("/create", (req, res) => {
 
     });
 
-    res.sendStatus(201);
+    res.redirect("/");
 });
 
 router.get("/edit/:id", (req, res) => {
-    res.send("EDIT");
+    console.log(req.params.id);
+    let model = new customerService();
+    data.map((row, index) => {
+        let id = Number.parseInt(row.id);
+        if (id == req.params.id) {
+        console.log("!");
+            
+            model = row;
+        }
+    });
+        console.log(model);
+    
+    res.render('pages/edit', { data: model });
 });
 
 router.put("/edit", (req, res) => {
@@ -57,9 +70,9 @@ router.delete("/delete", (req, res) => {
 });
 
 router.get("/view/:id", (req, res) => {
-    
+
     let id = req.params.id;
-    
+
     res.send("VIEW" + id);
 });
 
